5fcfbe76e653095e147e4aa43a63f9484595713c
Omar Zuhri <omar.z.zuhri@gmail.com>
2026-08-13 15:42:24 +0300

    feat: implement Inertia.js setup for React application

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,1 +1,28 @@
+import '../css/app.css';
+import './bootstrap';
 
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
+
+const appName = import.meta.env.VITE_APP_NAME || 'LCMS';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx'),
+        ),
+
+    setup({ el, App, props }) {
+        createRoot(el).render(
+            <App {...props} />
+        );
+    },
+
+    progress: {
+        color: '#3F46D3',
+    },
+});
